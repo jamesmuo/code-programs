4ab47df73fe82783c13b8722490fde267f112320
jamesmuo <26312885+jamesmuo@users.noreply.github.com>
2021-05-07 09:18:43 -0500

    Create convertToBase64.js

---
+++ base64/convertToBase64.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require("fs");
+
+if (process.argv.length == 3) {
+    var imageData = fs.readFileSync(process.argv[2], "base64");
+    var ext = process.argv[2].substr(process.argv[2].lastIndexOf(".") + 1);
+    var outFile = process.argv[2].substr(0, process.argv[2].lastIndexOf(".")) + ".txt";
+    fs.writeFileSync(outFile, "data:image/" + ext + ";base64," + imageData);
+} else {
+    console.log("Error: please provide filename");
+    process.exit(1);
+}
